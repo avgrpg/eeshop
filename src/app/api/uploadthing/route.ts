b6dfea9fa7f356650040bbb344fa6e95705e7ae7
avgrpg <tsuiwaiwai2000@gmail.com>
2024-12-27 05:02:29 +0800

    fix onuploadcompolete err

--- src/app/api/uploadthing/route.ts
+++ src/app/api/uploadthing/route.ts
@@ -1,8 +1,10 @@
 import { createRouteHandler } from "uploadthing/next";
 
 import { ourFileRouter } from "./core";
+import { env } from "~/env";
 
 // Export routes for Next App Router
 export const { GET, POST } = createRouteHandler({
   router: ourFileRouter,
+  config: { isDev: env.NODE_ENV === "development" },
 });
