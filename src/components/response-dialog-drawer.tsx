3f0d46d4b62d0e3a8439ec16408f1442cb988cfa
avgrpg <tsuiwaiwai2000@gmail.com>
2025-01-28 13:03:40 +0800

    dialog consume product detaill

--- src/components/response-dialog-drawer.tsx
+++ src/components/response-dialog-drawer.tsx
@@ -15,6 +15,7 @@ import {
   Dialog,
   DialogContent,
   DialogDescription,
+  DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
@@ -31,6 +32,74 @@ interface productsWithImagesAndTagsWithSubcategory
   extends ProductWithImagesAndTags {
   subcategory: Subcategory | undefined;
 }
+
+const ProductDisplay = ({
+  product,
+}: {
+  product: productsWithImagesAndTagsWithSubcategory;
+}) => {
+  return (
+    <ScrollArea className="h-full px-5">
+      <div className="p-2">
+        <strong className="text-xl font-bold leading-none tracking-tight">
+          $ {product.price}
+        </strong>
+      </div>
+
+      {product.tags.length > 0 && (
+        <div className="flex flex-wrap gap-2 py-2">
+          {product.tags.map((tag) => (
+            <small
+              key={tag?.id}
+              className="truncate rounded-sm bg-muted px-2.5 py-1 text-sm font-medium leading-none text-muted-foreground"
+            >
+              {tag?.name}
+            </small>
+          ))}
+        </div>
+      )}
+
+      <ScrollArea className="grid h-44 w-full justify-center">
+        {product.images.length > 0 && (
+          <div className="flex gap-2 py-2">
+            {product.images.map((image) => (
+              <Image
+                key={image.url}
+                src={image.url}
+                alt={image.url}
+                width={200}
+                height={160}
+                className="h-40 w-auto rounded-sm"
+              />
+            ))}
+          </div>
+        )}
+        <ScrollBar orientation="horizontal" className="h-2" />
+      </ScrollArea>
+
+      <div className="pb-2 pt-4">
+        <p className="text-sm font-medium text-muted-foreground">
+          {product.description}
+        </p>
+      </div>
+
+      <div className="mt-5 flex flex-col gap-2 border-t-2 border-border py-2 font-medium text-muted-foreground">
+        <div className="flex flex-col gap-2 py-1">
+          <p className="font-medium text-foreground">Category:</p>
+          <span className="pl-3 text-sm font-medium text-muted-foreground">
+            {product.subcategory?.category?.name}
+          </span>
+        </div>
+        <div className="flex flex-col gap-2 py-1">
+          <p className="font-medium text-foreground">Subcategory:</p>
+          <span className="pl-3 text-sm font-medium text-muted-foreground">
+            {product.subcategory?.name}
+          </span>
+        </div>
+      </div>
+    </ScrollArea>
+  );
+};
 
 export function ResponseDialogDrawer({
   children,
@@ -62,9 +131,9 @@ export function ResponseDialogDrawer({
             />
           )}
           <div className="fixed top-0 flex h-1/5 max-w-[85%] items-end py-4 pl-12 text-background">
-          <div className="flex items-center min-h-20">
-            <h2 className="text-2xl font-bold">{product.name}</h2>
-          </div>
+            <div className="flex min-h-20 items-center">
+              <h2 className="text-2xl font-bold">{product.name}</h2>
+            </div>
           </div>
           <div className="fixed top-0 px-4 py-5 text-background">
             <DrawerClose asChild>
@@ -76,65 +145,7 @@ export function ResponseDialogDrawer({
           </div>
         </DrawerOverlay>
         <DrawerContent className="h-4/5 rounded-t-2xl">
-          <ScrollArea className="h-full px-5">
-            <div className="p-2">
-              <strong className="text-xl font-bold leading-none tracking-tight">
-                $ {product.price}
-              </strong>
-            </div>
-
-            {product.tags.length > 0 && (
-              <div className="flex flex-wrap gap-2 py-2">
-                {product.tags.map((tag) => (
-                  <small
-                    key={tag?.id}
-                    className="truncate rounded-sm bg-muted px-2.5 py-1 text-sm font-medium leading-none text-muted-foreground"
-                  >
-                    {tag?.name}
-                  </small>
-                ))}
-              </div>
-            )}
-
-            <ScrollArea className="grid h-44 w-full justify-center">
-              {product.images.length > 0 && (
-                <div className="flex gap-2 py-2">
-                  {product.images.map((image) => (
-                    <Image
-                      key={image.url}
-                      src={image.url}
-                      alt={image.url}
-                      width={200}
-                      height={160}
-                      className="h-40 w-auto rounded-sm"
-                    />
-                  ))}
-                </div>
-              )}
-              <ScrollBar orientation="horizontal" className="h-2" />
-            </ScrollArea>
-
-            <div className="pb-2 pt-4">
-              <p className="text-sm font-medium text-muted-foreground">
-                {product.description}
-              </p>
-            </div>
-
-            <div className="mt-5 flex flex-col gap-2 border-t-2 border-border py-2 font-medium text-muted-foreground">
-              <div className="flex flex-col gap-2 py-1">
-                <p className="font-medium text-foreground">Category:</p>
-                <span className="pl-3 text-sm font-medium text-muted-foreground">
-                  {product.subcategory?.category?.name}
-                </span>
-              </div>
-              <div className="flex flex-col gap-2 py-1">
-                <p className="font-medium text-foreground">Subcategory:</p>
-                <span className="pl-3 text-sm font-medium text-muted-foreground">
-                  {product.subcategory?.name}
-                </span>
-              </div>
-            </div>
-          </ScrollArea>
+          <ProductDisplay product={product} />
           <DrawerFooter className="flex items-center pb-6">
             <div>
               <Button variant="outline" className="w-auto">
@@ -152,14 +163,23 @@ export function ResponseDialogDrawer({
       <DialogTrigger asChild>
         <div>{children}</div>
       </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle>Edit profile</DialogTitle>
-          <DialogDescription>
-            Make changes to your profile here. Click save when youa re done.
+      <DialogContent className="min-w-[80vw]">
+        <DialogHeader className="p-6">
+          <DialogTitle>
+            {product.name}
+          </DialogTitle>
+          <DialogDescription className="sr-only">
+            {product.description}
           </DialogDescription>
         </DialogHeader>
-        sth
+        <ProductDisplay product={product} />
+        <DialogFooter className="flex justify-center items-center pb-6 w-full sm:justify-center">
+          <div>
+            <Button variant="outline" className="w-auto">
+              How to Buy
+            </Button>
+          </div>
+        </DialogFooter>
       </DialogContent>
     </Dialog>
   );
